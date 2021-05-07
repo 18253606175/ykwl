4f5820ad3ac7d7c583d71b83b64e14b4219ef701
unknown <2586899411@qq.com>
2021-05-07 14:18:01 +0800

    第六次提交

--- js/deviceList.js
+++ js/deviceList.js
@@ -18,6 +18,9 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
         //标注选中样式
         obj.tr.addClass('layui-table-click').siblings().removeClass('layui-table-click');
 
+
+
+
         layer.open({
             type: 2,
             id: "iframe",
@@ -34,6 +37,175 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
         });
 
 
+    });
+
+    $("#pop-up-add").append(
+        `
+        <form class="layui-form" action="">
+        <div class="layui-form-item">
+            <label class="layui-form-label layui-required">设备类型</label>
+            <div class="layui-input-block">
+                <select name="modules" lay-verify="required" lay-search="">
+                    <option value=""></option>
+                    <option value="0">北京</option>
+                    <option value="1">上海</option>
+                    <option value="2">广州</option>
+                    <option value="3">深圳</option>
+                    <option value="4">杭州</option>
+                </select>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label layui-required">设备编号</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" lay-verify="required" placeholder="请输入" autocomplete="off"
+                    class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label layui-required">所属楼层</label>
+            <div class="layui-input-block">
+                <select name="modules" lay-verify="required" lay-search="">
+                    <option value=""></option>
+                    <option value="0">-3</option>
+                    <option value="1">-2</option>
+                    <option value="2">-1</option>
+                    <option value="3">1</option>
+                    <option value="4">2</option>
+                </select>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label layui-required">位置描述</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" lay-verify="required" placeholder="请输入" autocomplete="off"
+                    class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">安装日期</label>
+            <div class="layui-input-block">
+                <input type="text" name="date" id="date" lay-verify="date" placeholder="请选择安装日期" autocomplete="off"
+                    class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">物联卡号</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" placeholder="请输入" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">设备名称</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" placeholder="请输入" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item  layui-form-item-submit">
+            <div class="layui-input-block">
+                <button type="submit" class="layui-btn" lay-submit="" lay-filter="demo1">确认</button>
+            </div>
+        </div>
+    </form>
+    <button id="close-pop-up" class="layui-btn">取消</button>
+        `
+
+
+    )
+
+
+    //表格编辑删除
+    table.on('tool(tableTest)', function (obj) {
+        var data = obj.data;
+        //表格编辑删除
+        if (obj.event === 'del') {
+            layer.confirm('真的删除行么', function (index) {
+                obj.del();
+                layer.close(index);
+            });
+        } else if (obj.event === 'edit') {
+            layer.open({
+                type: 1,
+                offset: '180px',
+                title: '添加设备',
+                area: ['1000px', '400px'],
+                content: $("#pop-up-add"),
+                success: function (layero, index) {
+                    $("#pop-up-add").html(
+                        `
+                <form class="layui-form" action="">
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-required">设备类型</label>
+                    <div class="layui-input-block">
+                        <select name="modules" lay-verify="required" lay-search="">
+                            <option value=""></option>
+                            <option value="0">北京</option>
+                            <option value="1">上海</option>
+                            <option value="2">广州</option>
+                            <option value="3">深圳</option>
+                            <option value="4">杭州</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-required">设备编号</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="title" lay-verify="required" disabled placeholder="请输入" autocomplete="off"
+                            class="layui-input" value=${data.deviceId}>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-required">所属楼层</label>
+                    <div class="layui-input-block">
+                        <select name="modules" lay-verify="required" lay-search="">
+                            <option value=""></option>
+                            <option value="0">-3</option>
+                            <option value="1">-2</option>
+                            <option value="2">-1</option>
+                            <option value="3">1</option>
+                            <option value="4">2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-required">位置描述</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="title" lay-verify="required" placeholder="请输入" autocomplete="off"
+                            class="layui-input" value=${data.location}>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">安装日期</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="date" disabled id="date" lay-verify="date" placeholder="请选择安装日期" autocomplete="off"
+                            class="layui-input" value=${data.installationTime}>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">SIM卡号</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="title" placeholder="请输入" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">设备名称</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="title" placeholder="请输入" autocomplete="off" class="layui-input" value=${data.deviceName}>
+                    </div>
+                </div>
+                <div class="layui-form-item  layui-form-item-submit">
+                    <div class="layui-input-block">
+                        <button type="submit" class="layui-btn" lay-submit="" lay-filter="demo1">确认</button>
+                    </div>
+                </div>
+            </form>
+            <button id="close-pop-up" class="layui-btn">取消</button>
+        `
+                    )
+                    form.render()
+                }
+            });
+        }
     });
     //头部内容
 
@@ -59,17 +231,16 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
 
     //下拉框value
     var selectDate = [{
-            title: '没有选项',
-            value: " "
-        }
-    ]
+        title: '没有选项',
+        value: " "
+    }]
 
 
     //下拉框请求
     $.ajax({
         url: baseUrl + "/company/ztreelist?token=" + JSON.parse(localStorage.getItem('loginInfo')).token,
-        success: function(res){
-            var selectDate1 = res.rows.map(item=>{
+        success: function (res) {
+            var selectDate1 = res.rows.map(item => {
                 return {
                     title: item.companyName,
                     value: item.id
@@ -82,82 +253,29 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
                     <option value=${item.value}>${item.title}</option>
                 `
             })
-        
+
             $(".layui-input-inline-select").html(selectMap.join(''));
             form.render();
         }
     })
 
 
-    
+
     $(".Realtime-left-top-content").append($p);
 
 
     //监听提交搜索
     form.on('submit(submitBtn)', function (data) {
-        table.render({
-            elem: '#home',
-            height: 740,
-            url: baseUrl + "/device/tablelist?token=" + JSON.parse(localStorage.getItem('loginInfo')).token,
+
+        table.reload('tableReload', {
+            page: {
+                curr: 1 //重新从第 1 页开始
+            },
             where: {
                 location: data.field.location,
                 deviceSmallType: typeId,
                 state: state
-            },
-            limit: 15,
-            limits: [15, 30, 45],
-            cellMinWidth: 85,
-            page: { //支持传入 laypage 组件的所有参数（某些参数除外，如：jump/elem） - 详见文档  
-                groups: 5 //只显示 1 个连续页码
-                    ,
-                first: false //不显示首页
-                    ,
-                last: false //不显示尾页
-            },
-            even: true,
-            parseData: function (res) { //res 即为原始返回的数据
-                return {
-                    "code": 0, //解析接口状态
-                    "data": res.rows.rows, //解析数据列表
-                    "count": res.rows.total,
-                };
-            },
-            request: {
-                pageName: 'pageNumber' //页码的参数名称，默认：page
-                    ,
-                limitName: 'pageSize' //每页数据量的参数名，默认：limit
-            },
-            cols: [
-                [ //表头
-                    {
-                        field: 'type',
-                        title: '状态',
-                        width: 60,
-                        templet: "#sexTpl"
-                    },
-                    {
-                        field: 'imei',
-                        title: '设备编号'
-                    },
-                    {
-                        field: 'deviceName',
-                        title: '设备名称'
-                    },
-                    {
-                        field: 'type',
-                        title: '设备类型',
-                        templet: "#typeSign"
-                    },
-                    {
-                        field: 'location',
-                        title: '设备安装地点'
-                    },
-                    {
-                        field: 'installationTime',
-                        title: '设备安装时间'
-                    },
-                ]
-            ]
+            }
         });
 
 
@@ -166,20 +284,20 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
 
     form.on('submit(submitDoubleBtn)', function (data) {
 
-        
+
 
         // console.log(data.field)
 
         table.reload('tableReload', {
             page: {
-              curr: 1 //重新从第 1 页开始
+                curr: 1 //重新从第 1 页开始
             },
             where: {
                 companyId: data.field.companyId
             }
-          });
+        });
 
-        
+
         return false;
     });
 
@@ -196,8 +314,8 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
             </div>
             <div>
                 <p>
-                    <span style="background-color: #319499;"></span>
-                    <p style="color: #319499;">在线：${titleBottom.online}</p>
+                    <span style="background-color: #3bd83f;"></span>
+                    <p style="color: #3bd83f;">在线：${titleBottom.online}</p>
                 </p>
                 <p>
                     <span style="background-color: #b82e00;"></span>
@@ -228,10 +346,12 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
                 return item.type_sign
             })
 
-            var leftTypeDate = leftType.map((item, index) => {
+            console.log(leftType)
+
+            var leftTypeDate = rows.map((item, index) => {
 
                 return `
-                        <li class="layui-nav-item select-li"><a href="javaScript:;">${item}<blockquote class="layui-elem-quote layui-quote-nm"><span class="layui-badge">${typeCount[index]}</span></blockquote> </a></li>
+                        <li class="layui-nav-item select-li" id=${item.type}><a href="javaScript:;">${item.type_sign}<blockquote class="layui-elem-quote layui-quote-nm"><span class="layui-badge">${typeCount[index]}</span></blockquote> </a></li>
                     `
 
             })
@@ -246,32 +366,22 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
                     // for (var j = 0; j < $(".Realtime-left-bottom > .layui-nav > li").length - 1; j++) {
                     //     $(".wisdom-electricity-bottom > div")[j].className = "";
                     // }
-                    typeSum = Number(this.getAttribute('index')) + 1
-                    if (typeSum === 1) {
-                        typeId = '01'
-                    } else if (typeSum === 2) {
-                        typeId = '02'
-                    } else if (typeSum === 3) {
-                        typeId = '03'
-                    } else if (typeSum === 4) {
-                        typeId = '06'
-                    } else if (typeSum === 5) {
-                        typeId = '05'
-                    }
+                    typeId = this.getAttribute('id')
+                    console.log(typeId)
 
 
                     table.reload('tableReload', {
                         page: {
-                          curr: 1 //重新从第 1 页开始
+                            curr: 1 //重新从第 1 页开始
                         },
                         where: {
                             state: state,
                             deviceSmallType: typeId
                         }
-                      });
+                    });
 
 
-                    
+
                     // $(".wisdom-electricity-bottom > div")[Number(this.getAttribute("index")) + 1].className = "current";
 
                     //小类型状态筛选
@@ -297,16 +407,16 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
 
                             table.reload('tableReload', {
                                 page: {
-                                  curr: 1 //重新从第 1 页开始
+                                    curr: 1 //重新从第 1 页开始
                                 },
                                 where: {
                                     state: state,
                                     deviceSmallType: typeId
                                 }
-                              });
+                            });
 
-                            
-                            
+
+
                         }
                     }
                 }
@@ -351,7 +461,7 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
     $(".wisdom-electricity-bottom-top-classify").append(
         `
         <p class="classifyStyle typeScreen"><span style="background: #1191da;"></span>全部</p>
-        <p class="typeScreen"><span style="background: #184f6d;"></span> 正常</p>
+        <p class="typeScreen"><span style="background: #3bd83f;"></span> 正常</p>
         <p class="typeScreen"><span style="background: #c82c1f"></span> 报警</p> 
         <p class="typeScreen"><span style="background: #bf671d"></span> 故障</p> 
         <p class="typeScreen"><span style="background: #75747c"></span> 离线</p> 
@@ -389,24 +499,24 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
 
             table.reload('tableReload', {
                 page: {
-                  curr: 1 //重新从第 1 页开始
+                    curr: 1 //重新从第 1 页开始
                 },
                 where: {
                     state: state,
                     deviceSmallType: typeId
                 }
-              });
+            });
 
-            
+
         }
     }
 
     table.render({
         elem: '#home',
         id: 'tableReload',
-        height: 740,
+        height: 780,
         url: baseUrl + "/device/tablelist?token=" + JSON.parse(localStorage.getItem('loginInfo')).token,
-        where:{
+        where: {
             state: state
         },
         limit: 15,
@@ -420,6 +530,7 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
             last: false //不显示尾页
         },
         even: true,
+        skin: 'row',
         parseData: function (res) { //res 即为原始返回的数据
             return {
                 "code": 0, //解析接口状态
@@ -445,10 +556,6 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
                     title: '设备编号'
                 },
                 {
-                    field: 'deviceName',
-                    title: '设备名称'
-                },
-                {
                     field: 'type',
                     title: '设备类型',
                     templet: "#typeSign"
@@ -461,6 +568,13 @@ layui.use(['element', 'layer', 'table', 'form'], function () {
                     field: 'installationTime',
                     title: '设备安装时间'
                 },
+                {
+                    fixed: 'right',
+                    title: '操作',
+                    toolbar: '#barDemo',
+                    width: 150,
+                    toolbar: '#barDemo'
+                }
             ]
         ]
     });
